7b78c34072e48546c2787c778026131e22c93e19
Michael Liu <bbz975749@gmail.com>
2024-10-15 17:04:32 +0800

    HW4

---
+++ HW4/FileBrowser/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace FileBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Filename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Path = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1393, 53);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(2, 59);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1393, 60);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(2, 125);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1393, 786);
+            this.splitContainer1.SplitterDistance = 464;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(464, 786);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Filename,
+            this.Size,
+            this.Path});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(925, 786);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Filename
+            // 
+            this.Filename.Text = "Filename";
+            this.Filename.Width = 190;
+            // 
+            // Size
+            // 
+            this.Size.Text = "Size";
+            this.Size.Width = 198;
+            // 
+            // Path
+            // 
+            this.Path.Text = "Path";
+            this.Path.Width = 199;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "菜单";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "工具栏";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "排序";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(395, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "查看";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1397, 914);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Filename;
+        private System.Windows.Forms.ColumnHeader Size;
+        private System.Windows.Forms.ColumnHeader Path;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ HW4/FileBrowser/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FillRootTreeView(treeView1, @"C:\test");
+
+        }
+        private void FillRootTreeView(TreeView treeView, string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            DirectoryInfo rootdir = new DirectoryInfo(@"C:\root");
+            TreeNode node = new TreeNode(rootdir.Name);
+            node.Tag = dir.FullName;
+            treeView.Nodes.Add(node);
+            FillSubTreeView(treeView, dir.FullName, node);
+        }
+        private void FillSubTreeView(TreeView treeView, string path,TreeNode parent)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                TreeNode node = new TreeNode(dir.Name);
+                node.Tag = dir.FullName;
+                parent.Nodes.Add(node); // 将根节点添加到TreeView中
+
+                foreach (DirectoryInfo subDir in dir.GetDirectories())
+                {
+                    TreeNode subNode = new TreeNode(subDir.Name);
+                    subNode.Tag = subDir.FullName;
+                    FillSubTreeView(treeView, subDir.FullName,node); // 递归填充子节点
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("Error accessing directory: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listView1.Items.Clear();
+            DirectoryInfo dirInfo = new DirectoryInfo(e.Node.Tag.ToString());
+            foreach (FileInfo file in dirInfo.GetFiles())
+            {
+                ListViewItem item = new ListViewItem(file.Name);
+                item.SubItems.Add(file.Length.ToString());
+                item.SubItems.Add(file.FullName); // 添加完整路径到第三列
+                item.Tag = file.FullName;
+                listView1.Items.Add(item);
+            }
+            foreach (DirectoryInfo subDir in dirInfo.GetDirectories())
+            {
+                ListViewItem item = new ListViewItem(subDir.Name);
+                item.SubItems.Add("Folder");
+                item.Tag = subDir.FullName;
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 获取当前选中的项
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+
+                // 获取文件路径
+                string filePath = selectedItem.Tag.ToString();
+
+                // 检查文件扩展名是否为.txt或.exe
+                if (filePath.EndsWith(".txt", StringComparison.OrdinalIgnoreCase) || filePath.EndsWith(".exe", StringComparison.OrdinalIgnoreCase))
+                {
+                    try
+                    {
+                        // 如果是.txt文件，使用记事本打开
+                        if (filePath.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Process.Start("notepad.exe", filePath);
+                        }
+                        // 如果是.exe文件，直接运行
+                        else if (filePath.EndsWith(".exe", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Process.Start(filePath);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error opening file: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("This operation is only allowed for .txt and .exe files.");
+                }
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 清除现有的节点
+            treeView1.Nodes.Clear();
+
+            // 重新加载根节点
+            FillRootTreeView(treeView1, @"C:\test");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 对ListView进行排序，默认按照第一列（Filename）进行升序排序
+            if (listView1.View == View.Details)
+            {
+                listView1.Sorting = SortOrder.Ascending;
+                listView1.Sort();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            switch (listView1.View)
+            {
+                case View.Details:
+                    listView1.View = View.LargeIcon;
+                    break;
+                case View.LargeIcon:
+                    listView1.View = View.SmallIcon;
+                    break;
+                case View.SmallIcon:
+                    listView1.View = View.List;
+                    break;
+                case View.List:
+                    listView1.View = View.Details;
+                    break;
+                default:
+                    listView1.View = View.Details;
+                    break;
+            }
+        }
+    }
+}
