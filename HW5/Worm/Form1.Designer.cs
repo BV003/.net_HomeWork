8e9511f32b0a2f3301fe0bcc05f366317491bd77
Michael Liu <bbz975749@gmail.com>
2024-10-24 09:58:22 +0800

    HW5

---
+++ HW5/Worm/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Worm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblPhoneCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "关键字";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 130);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(310, 87);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(819, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "搜索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 440);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "电话号码";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(195, 440);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(945, 292);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Url";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(302, 259);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(286, 148);
+            this.listBox1.TabIndex = 9;
+            // 
+            // lblPhoneCount
+            // 
+            this.lblPhoneCount.AutoSize = true;
+            this.lblPhoneCount.Location = new System.Drawing.Point(841, 315);
+            this.lblPhoneCount.Name = "lblPhoneCount";
+            this.lblPhoneCount.Size = new System.Drawing.Size(0, 18);
+            this.lblPhoneCount.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 791);
+            this.Controls.Add(this.lblPhoneCount);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblPhoneCount;
+    }
+}
+
---
+++ HW5/Worm/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Worm
+{
+    public partial class Form1 : Form
+    {
+        private HashSet<string> phoneNumbers = new HashSet<string>();  // 存储去重后的电话号码
+        private Dictionary<string, HashSet<string>> phoneNumberUrls = new Dictionary<string, HashSet<string>>();  // 电话号码及其关联的URLs
+        private IWebDriver webDriver;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // 初始化ChromeDriver
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless"); // 如果不想显示浏览器窗口，可以添加此行
+            webDriver = new ChromeDriver(options);
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string keyword = textBox1.Text.Trim();
+            if (string.IsNullOrEmpty(keyword))
+            {
+                MessageBox.Show("请输入搜索关键字！");
+                return;
+            }
+
+            listBox1.Items.Clear();
+            phoneNumbers.Clear();
+            phoneNumberUrls.Clear();
+            lblPhoneCount.Text = "已爬取到电话号码: 0";  // 初始化显示
+
+            // 进行异步搜索和网页爬取
+            await SearchAndCrawlAsync(keyword);
+
+            MessageBox.Show("爬取完成！");
+
+        }
+        private async Task SearchAndCrawlAsync(string keyword)
+        {
+            List<string> searchEngines = new List<string>
+            {
+                $"https://www.baidu.com/s?wd={keyword}"
+            };
+
+            List<Task> tasks = new List<Task>();
+
+            foreach (var engine in searchEngines)
+            {
+                tasks.Add(Task.Run(() => CrawlSearchResults(engine)));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        // 爬取每个搜索引擎的搜索结果
+        private void CrawlSearchResults(string url)
+        {
+            try
+            {
+                webDriver.Navigate().GoToUrl(url);  // 使用Selenium加载页面
+                var pageSource = webDriver.PageSource;  // 获取页面源代码
+                var urls = ExtractUrlsFromSearchPage(pageSource);
+
+                foreach (var targetUrl in urls)
+                {
+                    CrawlPhoneNumbersFromUrl(targetUrl);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error crawling {url}: {ex.Message}");
+            }
+        }
+
+        // 爬取特定URL并提取电话号码
+        private void CrawlPhoneNumbersFromUrl(string url)
+        {
+            if (phoneNumbers.Count >= 100) return;
+
+            try
+            {
+                webDriver.Navigate().GoToUrl(url);  // 使用Selenium加载页面
+                var pageSource = webDriver.PageSource;  // 获取页面源代码
+
+                var phoneNumbersInPage = ExtractPhoneNumbers(pageSource);
+
+                lock (phoneNumbers)
+                {
+                    foreach (var phoneNumber in phoneNumbersInPage)
+                    {
+                        if (phoneNumbers.Count >= 100) break; // 如果达到100个号码，停止添加
+
+                        if (phoneNumbers.Add(phoneNumber))
+                        {
+                            phoneNumberUrls[phoneNumber] = new HashSet<string> { url };  // 确保URL不重复
+                            UpdatePhoneCount(phoneNumber);
+                        }
+                        else
+                        {
+                            phoneNumberUrls[phoneNumber].Add(url);  // 只添加新的URL
+                        }
+                    }
+                }
+
+                if (phoneNumbers.Count >= 100) return;
+
+                Invoke(new Action(() => listBox1.Items.Add(url))); // 更新UI显示已爬取的URL
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error crawling {url}: {ex.Message}");
+            }
+        }
+
+        // 更新UI中电话号码个数的显示
+        private void UpdatePhoneCount(string phoneNumber)
+        {
+            Invoke(new Action(() =>
+            {
+                lblPhoneCount.Text = $"已爬取到电话号码: {phoneNumbers.Count}";
+                textBox2.AppendText($"{phoneNumber}\r\n"); // 添加到 textBox2
+            }));
+        }
+
+        // 提取页面中的电话号码
+        private List<string> ExtractPhoneNumbers(string html)
+        {
+            var phoneNumbers = new List<string>();
+            var regex = new Regex(@"\b\d{3,4}[-.\s]?\d{7,8}\b");  // 简单的电话号码匹配规则
+
+            var matches = regex.Matches(html);
+            foreach (Match match in matches)
+            {
+                phoneNumbers.Add(match.Value);
+            }
+
+            return phoneNumbers;
+        }
+
+        // 从搜索页面提取所有结果的URLs
+        private List<string> ExtractUrlsFromSearchPage(string html)
+        {
+            var urls = new HashSet<string>(); // 使用 HashSet 来自动处理重复项
+            var regex = new Regex(@"https?://[^\s""<>]+"); // 匹配以 http 或 https 开头的 URL
+
+            // 使用正则表达式查找所有匹配的 URL
+            var matches = regex.Matches(html);
+
+            // 将匹配到的 URL 添加到 HashSet 中
+            foreach (Match match in matches)
+            {
+                urls.Add(match.Value);
+            }
+
+            return new List<string>(urls); // 将 HashSet 转换回 List 返回
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            webDriver.Quit(); // 关闭浏览器
+        }
+    }
+
+}
+
